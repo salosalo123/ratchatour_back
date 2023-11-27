e89ea598519754daaf9d44f0e1157000ba0e4a07
Zeizvant <oxanashvilizezva@gmail.com>
2023-11-27 18:07:42 +0400

    add register  functionality

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/test', [RegisterController::class, 'test']);
+Route::post('/register', [RegisterController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
